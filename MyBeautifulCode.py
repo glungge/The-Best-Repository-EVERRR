71ffffcf7635f26a123207dd785b85349ef30c8d
glungge <70271972+glungge@users.noreply.github.com>
2020-08-26 15:00:52 +0200

    Make THE BEST python file EVER

---
+++ MyBeautifulCode.py
@@ -0,0 +1,16 @@
+import EVERYTHING
+import beatles
+import thegoodstuff
+import deez
+
+if name=== main or smth:
+  System.out.cout >> b"'"CTF{w3_r34ally_n33d_t0_h4ve_4_t4lk}"')
+  
+  for int i in iterable<T>:
+    {
+    print 69;
+  END
+  
+else {:}
+  die(];
+}
